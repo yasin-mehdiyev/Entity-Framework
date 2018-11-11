dfc51ca279c54e38f64a2fb58881d9aef9fde038
Yasin <Yasin@DESKTOP-JR5VDO0>
2018-11-11 10:24:37 +0400

    thecodding

---
+++ 10_11_2018/10_11_2018/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace _10_11_2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwStudents = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtPhone = new System.Windows.Forms.TextBox();
+            this.cmbGroups = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbWoman = new System.Windows.Forms.RadioButton();
+            this.rdbMan = new System.Windows.Forms.RadioButton();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudents)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgwStudents
+            // 
+            this.dgwStudents.AllowUserToAddRows = false;
+            this.dgwStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgwStudents.Location = new System.Drawing.Point(416, 12);
+            this.dgwStudents.Name = "dgwStudents";
+            this.dgwStudents.RowTemplate.Height = 28;
+            this.dgwStudents.Size = new System.Drawing.Size(848, 604);
+            this.dgwStudents.TabIndex = 0;
+            this.dgwStudents.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgwStudents_RowHeaderMouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ad Soyad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Telefon";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Cinsiyyət";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Qrup Nömrəsi";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Doğum Tarixi";
+            this.Column6.Name = "Column6";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtName.Location = new System.Drawing.Point(12, 61);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(252, 30);
+            this.TxtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad";
+            // 
+            // TxtSurname
+            // 
+            this.TxtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSurname.Location = new System.Drawing.Point(12, 147);
+            this.TxtSurname.Name = "TxtSurname";
+            this.TxtSurname.Size = new System.Drawing.Size(251, 30);
+            this.TxtSurname.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Telefon";
+            // 
+            // TxtPhone
+            // 
+            this.TxtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPhone.Location = new System.Drawing.Point(11, 236);
+            this.TxtPhone.Name = "TxtPhone";
+            this.TxtPhone.Size = new System.Drawing.Size(252, 30);
+            this.TxtPhone.TabIndex = 2;
+            // 
+            // cmbGroups
+            // 
+            this.cmbGroups.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbGroups.FormattingEnabled = true;
+            this.cmbGroups.Location = new System.Drawing.Point(12, 447);
+            this.cmbGroups.Name = "cmbGroups";
+            this.cmbGroups.Size = new System.Drawing.Size(252, 33);
+            this.cmbGroups.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Qrup Nömrəsi";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbWoman);
+            this.groupBox1.Controls.Add(this.rdbMan);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 293);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cinsiyyət";
+            // 
+            // rdbWoman
+            // 
+            this.rdbWoman.AutoSize = true;
+            this.rdbWoman.Location = new System.Drawing.Point(144, 47);
+            this.rdbWoman.Name = "rdbWoman";
+            this.rdbWoman.Size = new System.Drawing.Size(90, 29);
+            this.rdbWoman.TabIndex = 0;
+            this.rdbWoman.Text = "Qadın";
+            this.rdbWoman.UseVisualStyleBackColor = true;
+            // 
+            // rdbMan
+            // 
+            this.rdbMan.AutoSize = true;
+            this.rdbMan.Location = new System.Drawing.Point(19, 47);
+            this.rdbMan.Name = "rdbMan";
+            this.rdbMan.Size = new System.Drawing.Size(69, 29);
+            this.rdbMan.TabIndex = 0;
+            this.rdbMan.Text = "Kişi";
+            this.rdbMan.UseVisualStyleBackColor = true;
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.CustomFormat = "dd.MM.yyyy";
+            this.dtpBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirthday.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBirthday.Location = new System.Drawing.Point(13, 532);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(251, 30);
+            this.dtpBirthday.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Doğum Tarixi";
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.Location = new System.Drawing.Point(65, 594);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(126, 50);
+            this.BtnAdd.TabIndex = 6;
+            this.BtnAdd.Text = "Əlavə Et";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(65, 594);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(126, 50);
+            this.btnupdate.TabIndex = 6;
+            this.btnupdate.Text = "Yenilə";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Visible = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(218, 594);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(126, 50);
+            this.btndelete.TabIndex = 6;
+            this.btndelete.Text = "Sil";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Visible = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 676);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbGroups);
+            this.Controls.Add(this.TxtPhone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgwStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudents)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtPhone;
+        private System.Windows.Forms.ComboBox cmbGroups;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbWoman;
+        private System.Windows.Forms.RadioButton rdbMan;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+    }
+}
+
---
+++ 10_11_2018/10_11_2018/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _10_11_2018.Models;
+
+namespace _10_11_2018
+{
+    public partial class Form1 : Form
+    {
+        private CaClassEntities db = new CaClassEntities();
+        private Student selectedId;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillGetData();
+            FillComboGroup();
+        }
+
+
+        private void FillGetData()
+        {
+            dgwStudents.Rows.Clear();
+            List<Student> students = db.Students.ToList();
+            foreach (Student stu in students)
+            {
+                dgwStudents.Rows.Add(stu.Id,
+                                     stu.Name + " " + stu.Surname,
+                                     stu.Phone,
+                                     stu.Gender ? "Kişi" : "Qadın",
+                                     stu.Group.Name + " (" + stu.Group.Teacher.FullName + ")",
+                                     stu.Birthday.ToString("dd.MM.yyyy"));
+            }
+        }
+
+        private void FillComboGroup()
+        {
+            List<Group> groups = db.Groups.OrderBy(g => g.Name).ToList();
+            foreach (Group group in groups)
+            {
+                cmbGroups.Items.Add(group.Name);
+            }
+        }
+
+
+        private void Reset()
+        {
+            FillGetData();
+            TxtName.Text = "";
+            TxtPhone.Text = "";
+            TxtSurname.Text = "";
+            rdbMan.Checked = true;
+            dtpBirthday.Value = DateTime.Now;
+            cmbGroups.Text = "";
+
+            btnupdate.Visible = false;
+            btndelete.Visible = false;
+            BtnAdd.Visible = true;
+
+        }
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Student student = new Student
+            {
+                Name = TxtName.Text,
+                Surname = TxtSurname.Text,
+                Phone = TxtPhone.Text,
+                Gender = Convert.ToBoolean(rdbMan.Checked ? 1 : 0),
+                Birthday = dtpBirthday.Value,
+                GroupId = db.Groups.FirstOrDefault(g => g.Name == cmbGroups.Text).Id,
+            };
+
+
+            db.Students.Add(student);
+            db.SaveChanges();
+
+            Reset();
+    }
+
+        private void dgwStudents_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            btnupdate.Visible = true;
+            btndelete.Visible = true;
+            BtnAdd.Visible = false;
+
+            int id = Convert.ToInt32(dgwStudents.Rows[e.RowIndex].Cells[0].Value.ToString());
+
+            this.selectedId = db.Students.Find(id);
+
+            TxtName.Text = selectedId.Name;
+            TxtSurname.Text = selectedId.Surname;
+            TxtPhone.Text = selectedId.Phone;
+            cmbGroups.Text = selectedId.Group.Name;
+            dtpBirthday.Value = selectedId.Birthday;
+            if (selectedId.Gender)
+            {
+                rdbMan.Checked = true;
+                rdbWoman.Checked = false;
+            }
+            else
+            {
+                rdbMan.Checked = false;
+                rdbWoman.Checked = true;
+            }
+        }
+
+
+        private void btndelete_Click(object sender,EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Silmək istəyirsinizmi?", "Silmə işləmi", MessageBoxButtons.OKCancel);
+            if (r == DialogResult.OK)
+            {
+                db.Students.Remove(this.selectedId);
+                db.SaveChanges();
+                Reset();
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            selectedId.Name = TxtName.Text;
+            selectedId.Surname = TxtSurname.Text;
+            selectedId.Phone = TxtPhone.Text;
+            selectedId.Gender = Convert.ToBoolean(rdbMan.Checked ? 1 : 0);
+            selectedId.Birthday = dtpBirthday.Value;
+            selectedId.GroupId = db.Groups.FirstOrDefault(g => g.Name == cmbGroups.Text).Id;
+
+            db.SaveChanges();
+            Reset();
+        }
+    }
+}
